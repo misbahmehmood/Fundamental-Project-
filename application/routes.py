9ca2d02971b0a1ac3a6b84ca0fa39cf7e5126748
misbaharmehmood <misbaharmehmood@gmail.com>
2020-05-21 12:06:04 +0000

    added new pages for different personalities

--- application/routes.py
+++ application/routes.py
@@ -18,26 +18,16 @@ def about():
 
 def quiz():
     dataform=PersonalityForm()
-    print('------------------------------', dataform.validate_on_submit(), '--------------------------------------')
     if request.method == 'POST':
-        print('------------------------------', dataform.options.data, '--------------------------------------')
         if dataform.options.data == 'Extravert':
-            return redirect(url_for('personality'))
-
-        
-    '''data= Personality(
-        personality_type=dataform.options.data
-    )
-    db.session.add(data)
-    db.session.commit()'''
-        
-    
-       
+            return redirect(url_for('extravert'))
+        else:
+            return redirect(url_for('introvert'))
 
     return render_template('quiz.html', title='Quiz', form=dataform)
 
-@app.route('/personality', methods=['GET', 'POST'])
-def personality():
+@app.route('/extravert', methods=['GET', 'POST'])
+def extravert():
         form=SongForm()
         if form.validate_on_submit():
             songData=Songs(
@@ -51,8 +41,7 @@ def personality():
             return redirect(url_for('home'))
         else:
             print(form.errors)
-        return render_template('personality.html', title='Songs', form=form)
-
+        return render_template('extravert.html', title='Songs', form=form)
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     to_update= Songs.query.get_or_404(id)
@@ -67,10 +56,7 @@ def update(id):
             db.session.commit()
             return redirect (url_for('home'))
 
-    return render_template('personality_new.html', title='Update', form=form, update=to_update)
-
-
-
+    return render_template('extravert_new.html', title='Update', form=form, update=to_update)
 
 @app.route('/delete/<int:id>')
 def delete(id):
@@ -81,3 +67,47 @@ def delete(id):
     return redirect(url_for('home'))
     
 
+@app.route('/introvert', methods=['GET', 'POST'])
+def introvert():
+        form=SongForm()
+        if form.validate_on_submit():
+            songData=Songs(
+                title=form.title.data,
+                artist=form.artist.data,
+                genre=form.genre.data,
+                instrument=form.instrument.data
+            )
+            db.session.add(songData)
+            db.session.commit()
+            return redirect(url_for('introvert_add'))
+        else:
+            print(form.errors)
+        return render_template('introvert.html', title='Your Songs', form=form)
+@app.route('/introvert/add')
+def introvert_add():
+    postData=Songs.query.all()
+    return render_template('introvert_add', title='See your Songs', songs=postData)
+
+@app.route('/introvert/update/<int:id>', methods=['GET', 'POST'])
+def introvert_update(id):
+    to_update= Songs.query.get_or_404(id)
+    form=UpdateForm()
+    if request.method == 'POST':
+        
+        if form.validate_on_submit():
+            to_update.title=form.title.data,
+            to_update.artist=form.artist.data,
+            to_update.genre=form.genre.data,
+            to_update.instrument=form.instrument.data
+            db.session.commit()
+            return redirect (url_for('introvert_add'))
+
+    return render_template('introvert_update.html', title='Update', form=form, update=to_update)
+
+@app.route('/introvert/delete/<int:id>')
+def introvert_delete(id):
+    to_delete= Songs.query.get_or_404(id)
+    
+    db.session.delete(to_delete)
+    db.session.commit()
+    return redirect(url_for('introvert_add'))
